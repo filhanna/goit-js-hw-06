bcafbd73091d0b16e101b078df85a6841e761af9
filhanna <filimonenkohanna@gmail.com>
2022-12-17 02:26:15 +0200

    task 4 completed

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const createGalleryItem = ({ url, alt }) =>
+  `<li><img src="${url}" alt="${alt}" width = 200 height = 150></li>`;
+const galleryList = images.reduce(
+  (previousValue, item) => previousValue + createGalleryItem(item),
+  ''
+);
+
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML('afterbegin', galleryList);
+list.setAttribute('style', 'list-style-type:none; display:flex; gap:10px');
---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const buttonPlus = document.querySelector("[data-action='decrement']");
+const buttonMinus = document.querySelector("[data-action='increment']");
+const value = document.querySelector('#value');
+
+let counterValue = 0;
+
+const increment = () => {
+  counterValue += 1;
+
+  value.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+
+  value.textContent = counterValue;
+};
+
+buttonPlus.addEventListener('click', decrement);
+buttonMinus.addEventListener('click', increment);
