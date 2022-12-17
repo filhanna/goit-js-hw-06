6eec62c07b4d2f5de8eda8aaeb9a232427d683a9
filhanna <filimonenkohanna@gmail.com>
2022-12-18 00:43:30 +0200

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+'use strict';
+
+const buttonChange = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+buttonChange.addEventListener('click', () => {
+  document.body.style.backgroundColor = color.textContent;
+  color.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
